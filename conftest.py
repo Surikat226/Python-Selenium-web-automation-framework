e32813f076f89bc10e15b3790dd087642340c65c
unknown <awf2014@yandex.ru>
2021-10-28 13:42:57 +0300

    ...

--- conftest.py
+++ conftest.py
@@ -2,11 +2,24 @@ import pytest
 from selenium import webdriver
 import time
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', \
+                     action='store', \
+                     default="chrome", \
+                     help="Choose browser: chrome or firefox")
+
 @pytest.fixture()
-def browser():
-    browser = webdriver.Chrome(executable_path="Диск:/Путь/К/Драйверу.exe")
-    browser.maximize_window()
-    browser.implicitly_wait(5)
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    if (browser_name == "chrome"):
+        browser = webdriver.Chrome()
+        browser.maximize_window()
+        browser.implicitly_wait(5)
+    elif (browser_name == "firefox"):
+        fp = webdriver.FirefoxProfile()
+        browser = webdriver.Firefox(firefox_profile=fp)
+        browser.maximize_window()
+        browser.implicitly_wait(5)
     yield browser
     time.sleep(3)
     browser.quit()
